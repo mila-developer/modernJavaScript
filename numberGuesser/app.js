1797ad35cabbc170723767cdb370bbc626d7e5d0
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-02-23 15:14:51 +0000

    ADD: play again and randomNum

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -24,6 +24,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e) {
+  if(e.target.className === 'play-again') {
+    window.location.reload();
+  }
+});
 
 // Listen for guess
 guessBtn.addEventListener('click', function() {
@@ -67,14 +74,23 @@ function gameOver(won, msg) {
   let color;
   won === true ? color = 'green' : color = 'red';
 
-   // Disable input
-   guessInput.disabled = true;
-   // Change border color
-   guessInput.style.borderColor = color;
-   // Set text color
-   message.style.color = color;
-   // Set message
-   setMessage(msg);
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+
+  // Play Again?
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
