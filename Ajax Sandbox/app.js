8d194d0cc247892f460e10e55733de0be7d51132
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-02-25 16:54:51 +0000

    ADD: customers

--- Ajax Sandbox/app.js
+++ Ajax Sandbox/app.js
@@ -1,22 +1,64 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
 
-function loadData() {
-  // Create an XHR Object
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer
+function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
-  // OPEN
-  xhr.open('GET', 'data.txt', true);
+  xhr.open('GET', 'customer.json', true);
 
-  xhr.onload = function() {
+  xhr.onload = function(){
     if(this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
     }
   }
 
   xhr.send();
+}
 
-  // HTTP Statuses
-  // 200: "OK"
-  // 403: "Forbidden"
-  // 404: "Not found"
+
+// Load Customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function(){
+    if(this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer){
+        output += `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
+    }
+  }
+
+  xhr.send();
 }
