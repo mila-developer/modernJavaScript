6d923667d2067de86e4941dd78b086787f662a41
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-02-28 16:45:17 +0000

    ADD: easyHTTP

---
+++ easyHTTP/app.js
@@ -0,0 +1,12 @@
+const http = new easyHTTP;
+
+// Get Posts
+http.get 
+('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+  console.log(posts);
+});
