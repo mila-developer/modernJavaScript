0402026e12a7c5d80444705d67b1d2798b35b4bf
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-03-02 12:07:09 +0000

    UPD: arrow function in the fetch api

--- Fetch API Sandbox/app.js
+++ Fetch API Sandbox/app.js
@@ -2,28 +2,22 @@ document.getElementById('button1').addEventListener('click', getText);
 document.getElementById('button2').addEventListener('click', getJson);
 document.getElementById('button3').addEventListener('click', getExternal);
 
-// Get local text file
+// Get local text file data
 function getText() {
   fetch('test.txt')
-  .then(function(res) {
-    return res.text();
-  })
-  .then(function(data) {
+  .then(res => res.text())
+  .then(data => {
     console.log(data);
     document.getElementById('output').innerHTML = data;
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 }
 
 // Get local json data
 function getJson() {
   fetch('posts.json')
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
+  .then(res => res.json())
+  .then(data => {
     console.log(data);
     let output = '';
     data.forEach(function(post) {
@@ -31,9 +25,7 @@ function getJson() {
     });
     document.getElementById('output').innerHTML = output;
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 }
 
 // Get from external API
