73605ead4d33495d6d7f722187e512cc3d417510
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-03-03 13:13:25 +0000

    UPD: github Finder

--- githubFinder/ui.js
+++ githubFinder/ui.js
@@ -34,6 +34,8 @@ class UI {
 
   // Show alert message
   showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
     // Create div
     const div = document.createElement('div');
     // Add classes
@@ -46,6 +48,45 @@ class UI {
     const search = document.querySelector('.search');
     // Insert alert
     container.insertBefore(div, search);
+
+    // Timeout after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 2000);
+  }
+
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo){
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers}</span>
+            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
   }
 
   // Clear profile
